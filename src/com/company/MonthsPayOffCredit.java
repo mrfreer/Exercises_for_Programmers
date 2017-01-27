41d38e1cf87f25e332a729a402ebd2fed59b4384
David <david.freer@gmail.com>
2017-01-27 00:32:19 -0500

    My confusion over this problem was over the math. http://stackoverflow.com/questions/32764833/credit-card-equation-in-c
    I was entering the credit card APR as a whole number instead of a decimal.

--- src/com/company/MonthsPayOffCredit.java
+++ src/com/company/MonthsPayOffCredit.java
@@ -7,32 +7,29 @@ import java.util.Scanner;
  */
 public class MonthsPayOffCredit {
     int n;
-    double i, b, p;
+    double i, b, p, calculation;
     public void readInput(){
         Scanner s = new Scanner(System.in);
         System.out.println("What is your balance?");
         while(!s.hasNextDouble()) s.next();
         b = s.nextDouble();
-        System.out.println("What is the APR (as a percent)?");
+        System.out.println("What is the APR (as a decimal)?");
         while(!s.hasNextDouble()) s.next();
         i = s.nextDouble();
         i /= 365.0;
         System.out.println("What is the monthly payment you can make?");
         while(!s.hasNextDouble()) s.next();
         p = s.nextDouble();
-        n = (int)((-1.0/30.0) * (Math.log((1 + (b/p)*(1-(Math.pow((1+i),30))))/(Math.log(1+i)))));
-        System.out.println(
-                (-1/30.0)*Math.log(1)
-        );
-        System.out.println("It will take you " + n + " months to pay off the card.");
+
+
+        calculation = (Math.log((1 + (double)(b / p) * (1 - (Math.pow(1 + (i), 30)))))) / (Math.log((1 + (i))));
+        double months = (-0.033333333333333333) * calculation;
+        int n = (int)Math.ceil(months);
+        System.out.println(n + " months to pay it off.");
     }
 
     public static void main(String[] args) {
-        //new MonthsPayOffCredit().readInput();
-        double i = 12.0/365.0;
-        System.out.println(
-                (-1.0/30.0)* (Math.log(1 + (5000.0/100.0) * (1-(Math.pow((1+i), 30)))) / (Math.log(1+i)))
-                //-1/30 x log(1+b/p(1-(1+i)^30)) / log(1+i)
-        );
+        new MonthsPayOffCredit().readInput();
+
     }
 }
