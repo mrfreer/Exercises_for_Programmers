f4633a32467a681493708faecf9744d89e10acff
shaggy0798 <david.freer@gmail.com>
2017-02-01 17:06:54 -0500

    Can read in multiple json objects.

--- src/main/java/com/company/ProductSearch.java
+++ src/main/java/com/company/ProductSearch.java
@@ -1,46 +1,60 @@
 package com.company;
 
+
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import jdk.nashorn.internal.parser.JSONParser;
+import org.json.simple.JSONObject;
+import org.json.simple.JsonArray;
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Wrapper;
+import java.util.*;
 
-class Person{
-    private String NAME;
-    private String P_LANGUAGE;
-    private String LOCATION;
+class Product{
+    public String name;
+    public double price;
+    public int quantity;
+
     public String toString(){
-        return NAME + " " + LOCATION;
+        return name + " Quantity: " + quantity + " Price $" + price;
     }
-    /*
-    public Person(String NAME, String P_LANGUAGE, String LOCATION){
-        this.NAME = NAME;
-        this.LOCATION = LOCATION;
-        this.P_LANGUAGE = P_LANGUAGE;
     }
-
-    public Person(){
-
-    }
-    */
-}
 /**
  * Created by dfreer on 2/1/2017.
  */
 public class ProductSearch{
 
     public void readIn() throws IOException{
+        String jsonString = new String(Files.readAllBytes(Paths.get("Data\\sample.json")),
+                StandardCharsets.UTF_8);
+        //System.out.println(jsonString);
         Gson gson = new Gson();
-        JsonElement json = gson.fromJson(new FileReader("Data\\sample.json"), JsonElement.class);
-        String result = gson.toJson(json);
-        System.out.println(result);
+        Product[] data = gson.fromJson(jsonString, Product[].class);
+        while(true){
+            System.out.println("Enter a product to search for:");
+            Scanner s = new Scanner(System.in);
+            String userInput = s.next();
+            for(Product p : data){
+                if(p.name.equals(userInput))
+                    System.out.println("Match -" +p.toString());
+            }
+            System.out.println("Enter quit to exit.");
+            if(s.next().equalsIgnoreCase("quit"))
+                break;
+        }
     }
 
     public static void main(String[] args) throws IOException {
-//reading in JSON files
+        //reading in JSON files
+
         new ProductSearch().readIn();
     }
 }
